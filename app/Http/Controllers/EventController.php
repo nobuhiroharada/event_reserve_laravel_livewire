efcf190d6b84a24e2edac17d517c28c317ac586a
Harada Nobuhiro <nobu@HaradanoMacBook-Air.local>
2023-06-04 20:41:48 +0900

    イベント更新画面追加

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -94,7 +94,12 @@ class EventController extends Controller
      */
     public function edit(Event $event)
     {
-        //
+        $event = Event::findOrFail($event->id);
+        $eventDate = $event->eventDate;
+        $startTime = $event->startTime;
+        $endTime = $event->endTime;
+
+        return view('manager.events.edit', compact('event'));
     }
 
     /**
