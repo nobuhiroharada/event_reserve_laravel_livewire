11a61c990881c6c121bfd0791447543f902a0a2f
Harada Nobuhiro <nobu@HaradanoMacBook-Air.local>
2023-06-05 21:38:25 +0900

    本日、過去イベント一覧に予約人数表示を追加

--- resources/views/manager/events/index.blade.php
+++ resources/views/manager/events/index.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            イベント管理
+            本日以降のイベント一覧
         </h2>
     </x-slot>
 
@@ -38,7 +38,13 @@
                                         <td class="text-blue-500 px-4 py-3"><a href="{{ route('events.show', [ 'event' => $event->id ]) }}">{{ $event->name }}</a></td>
                                         <td class="px-4 py-3">{{ $event->start_date }}</td>
                                         <td class="px-4 py-3">{{ $event->end_date }}</td>
-                                        <td class="px-4 py-3">Free</td>
+                                        <td class="px-4 py-3">
+                                            @if (is_null($event->number_of_people))
+                                                0
+                                            @else
+                                                {{ $event->number_of_people }}
+                                            @endif
+                                        </td>
                                         <td class="px-4 py-3">{{ $event->max_people }}</td>
                                         <td class="px-4 py-3">{{ $event->is_visible }}</td>
                                     </tr>
@@ -46,10 +52,6 @@
                             </tbody>
                         </table>
                         {{ $events->links() }}
-                    </div>
-                    <div class="flex pl-4 mt-4 lg:w-2/3 w-full mx-auto">
-                   
-                    
                     </div>
                 </div>
                 </section>
