56e938ee3534dadf11220c64d6a27f7ea9b192fd
Harada Nobuhiro <nobu@HaradanoMacBook-Air.local>
2023-06-07 21:23:18 +0900

    イベントのダミーデータ修正

--- database/factories/EventFactory.php
+++ database/factories/EventFactory.php
@@ -16,14 +16,22 @@ class EventFactory extends Factory
      */
     public function definition()
     {
-        $dummyDate = $this->faker->dateTimeThisMonth;
+        $availableHour = $this->faker->numberBetween(10, 18); // 10時〜18時
+        $minutes = [0, 30];
+        $mKey = array_rand($minutes);
+        $addHour = $this->faker->numberBetween(1, 3); // イベント時間 1時間~3時間
+
+        $dummyDate = $this->faker->dateTimeThisMonth; // 今月分をランダムに取得
+        $startDate = $dummyDate->setTime($availableHour, $minutes[$mKey]);
+        $clone = clone $startDate; // そのままmodifyすると$startDateも変わるためclone
+        $endDate = $clone->modify('+' . $addHour . 'hour');
 
         return [
             'name' => $this->faker->name,
             'information' => $this->faker->realText,
             'max_people' => $this->faker->numberBetween(1, 20),
-            'start_date' => $dummyDate->format('Y-m-d H:i:s'),
-            'end_date' => $dummyDate->modify('+1hour')->format('Y-m-d H:i:s'),
+            'start_date' => $startDate,
+            'end_date' => $endDate,
             'is_visible' => $this->faker->boolean,
         ];
     }
