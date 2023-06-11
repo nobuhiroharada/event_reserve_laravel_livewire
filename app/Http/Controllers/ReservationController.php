8514b5eef649b6191862622cedc755febb90f28a
Harada Nobuhiro <nobu@HaradanoMacBook-Air.local>
2023-06-11 21:43:42 +0900

    イベント予約可能人数表示

--- app/Http/Controllers/ReservationController.php
+++ app/Http/Controllers/ReservationController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use App\Models\Event;
 
 class ReservationController extends Controller
@@ -15,6 +16,20 @@ class ReservationController extends Controller
     public function detail($id)
     {
         $event = Event::findOrFail($id);
-        return view('event-detail', compact('event'));
+        // 予約可能な人数 = 最大定員 - 予約済みの人数(キャンセルを除く)
+        $reservedPeople = DB::table('reservations')
+            ->select('event_id', DB::raw('sum(number_of_people) as number_of_people'))
+            ->whereNull('canceled_date')
+            ->groupBy('event_id')
+            ->having('event_id', $event->id)
+            ->first();
+        
+        if(!is_null($reservedPeople)) {
+            $reservablePeople = $event->max_people - $reservedPeople->number_of_people;
+        } else {
+            $reservablePeople = $event->max_people;
+        }
+
+        return view('event-detail', compact('event', 'reservablePeople'));
     }
 }
