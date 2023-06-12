97fa2d123d65f9011d9489f245f891882c0b586a
Harada Nobuhiro <nobu@HaradanoMacBook-Air.local>
2023-06-12 22:18:10 +0900

    予約イベント取得処理追加

---
+++ app/Http/Controllers/MyPageController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+use App\Services\MyPageService;
+
+class MyPageController extends Controller
+{
+    public function index()
+    {
+        $user = User::findOrFail(Auth::id());
+        $events = $user->events;
+        $fromTodayEvents = MyPageService::reservedEvent($events, 'fromToday');
+        $pastEvents = MyPageService::reservedEvent($events, 'past');
+
+        return view('mypage/index', compact('fromTodayEvents', 'pastEvents'));
+    }
+}
